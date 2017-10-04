8b2d4b0c2969868540ec70a657fe8c4c171c3e77
Adrian Prisacaru <adi_prisacaru@yahoo.com>
2017-10-04 03:58:45 +0300

    handlebars template

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 require('rootpath')();
 const express = require('express');
+const path = require('path');
+const HBS = require('express-handlebars');
 
 const app = express();
 
+// Set handlebars as view engine
+const hbs = HBS.create({ extname: '.hbs' });
+app.engine('.hbs', hbs.engine);
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', '.hbs');
+
 app.use((req, res, next) => {
-    res.send('Hello');
+    res.render('index');
 });
 
 module.exports = app;
